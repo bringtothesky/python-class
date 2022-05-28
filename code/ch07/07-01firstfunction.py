da4c5c9c5126ec6169b397481c1c9c7ff061e406
bringtothesky <104672863+bringtothesky@users.noreply.github.com>
2022-05-28 18:19:16 +0900

    Create 07-01firstfunction.py

---
+++ code/ch07/07-01firstfunction.py
@@ -0,0 +1,14 @@
+def hello(): #함수 정의
+    print('\tHello, Python!')
+
+def helloint():
+    print('\t', 77)
+
+print('처음하는 함수 호출 hello()')
+hello() #함수 호출
+print('함수 호출 hello() 이후')
+
+print(1)
+print(helloint)
+helloint()
+print(3)
